d2ca64a401472c5eab3c93a8707ff42624d08595
GYP <2847399255@qq.com>
2021-12-26 11:11:02 +0800

    A

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import numpy
+import math
 from keras.preprocessing.image import save_img
 
 
@@ -9,34 +10,46 @@ def upAlign(x, y):
     return x + y - x % y
 
 
-def dct(timeDomain, frequencyDomain, x, y):
-    for i in range(0, 8):
-        for j in range(0, 8):
-            frequencyDomain[i][j] += 2 / 8
+def dct(time_domain, frequency_domain, x, y):
+    for u in range(0, 8):
+        for v in range(0, 8):
+            if u == 0 and v == 0:
+                c = 1 / 8
+            else:
+                c = 1 / 16
+            for i in range(0, 8):
+                for j in range(0, 8):
+                    frequency_domain[x + u][y + v] += \
+                        c * time_domain[x + i][y + j] * math.cos(math.pi * u * (2 * i + 1) / 16) * \
+                        math.cos(math.pi * v * (2 * j + 1) / 16)
 
 
-def encode(timeDomain, frequencyDomain):
-    for i in range(0, timeDomain.shape[0], 8):
-        for j in range(0, timeDomain.shape[0], 8):
-            dct(timeDomain, frequencyDomain, i, j)
-    dct(timeDomain, frequencyDomain)
+def encode(time_domain: numpy.ndarray, frequency_domain: numpy.ndarray) -> None:
+    for i in range(0, time_domain.shape[0], 8):
+        for j in range(0, time_domain.shape[0], 8):
+            dct(time_domain, frequency_domain, i, j)
+    dct(time_domain, frequency_domain)
 
 
-image = Image.open("images/image.bmp")
-data = numpy.asarray(image)
-print(type(data))
-Y = numpy.zeros([upAlign(len(data), 8), upAlign(len(data[0]), 8)], dtype=int)
-Cb = numpy.zeros([upAlign((len(data) + 1) // 2), upAlign((len(data[0]) + 1) // 2)], dtype=int)
-Cr = numpy.zeros([upAlign((len(data) + 1) // 2), upAlign((len(data[0]) + 1) // 2)], dtype=int)
-for i in range(0, len(data)):
-    for j in range(0, len(data[i])):
-        Y[i][j] = 0.229 * data[i][j][0] + 0.587 * data[i][j][1] + 0.114 * data[i][j][2]
-        if i % 2 == 0 and j % 2 == 0:
-            Cb[i // 2][j // 2] = -0.1687 * data[i][j][0] - 0.3313 * data[i][j][1] + 0.5 * data[i][j][2] + 128
-            Cr[i // 2][j // 2] = 0.5 * data[i][j][0] - 0.4187 * data[i][j][1] - 0.0813 * data[i][j][2] + 128
-Y1 = numpy.zeros([upAlign(len(data), 8), upAlign(len(data[0]), 8)], dtype=int)
-Cb1 = numpy.zeros([upAlign((len(data) + 1) // 2), upAlign((len(data[0]) + 1) // 2)], dtype=int)
-Cr1 = numpy.zeros([upAlign((len(data) + 1) // 2), upAlign((len(data[0]) + 1) // 2)], dtype=int)
-encode(Y, Y1)
+def main():
+    image = Image.open("images/image.bmp")
+    data = numpy.asarray(image)
+    print(type(data))
+    luminance_time_domain = numpy.zeros([upAlign(len(data), 8), upAlign(len(data[0]), 8)], dtype=int)
+    chrominance_time_domain = numpy.zeros([upAlign((len(data) + 1) // 2), upAlign((len(data[0]) + 1) // 2)], dtype=int)
+    saturation_time_domain = numpy.zeros([upAlign((len(data) + 1) // 2), upAlign((len(data[0]) + 1) // 2)], dtype=int)
+    for i in range(0, len(data)):
+        for j in range(0, len(data[i])):
+            luminance_time_domain[i][j] = 0.229 * data[i][j][0] + 0.587 * data[i][j][1] + 0.114 * data[i][j][2]
+            if i % 2 == 0 and j % 2 == 0:
+                chrominance_time_domain[i // 2][j // 2] = -0.1687 * data[i][j][0] - 0.3313 * data[i][j][1] + 0.5 * data[i][j][2] + 128
+                saturation_time_domain[i // 2][j // 2] = 0.5 * data[i][j][0] - 0.4187 * data[i][j][1] - 0.0813 * data[i][j][2] + 128
+    luminance_frequency_domain = numpy.zeros([upAlign(len(data), 8), upAlign(len(data[0]), 8)], dtype=int)
+    chrominance_frequency_domain = numpy.zeros([upAlign((len(data) + 1) // 2), upAlign((len(data[0]) + 1) // 2)], dtype=int)
+    saturation_frequency_domain = numpy.zeros([upAlign((len(data) + 1) // 2), upAlign((len(data[0]) + 1) // 2)], dtype=int)
+    encode(luminance_time_domain, luminance_frequency_domain)
+    save_img('images/out.bmp', luminance_time_domain)
 
-save_img('images/out.bmp', Y)
+
+if __file__ == 'main':
+    main()
