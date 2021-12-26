7f6e67279bc3888991ee8672e2f10b7858c4a052
gyp <2847399255@qq.com>
2021-12-26 18:31:17 +0800

    D

--- main.py
+++ main.py
@@ -32,10 +32,23 @@ def quantification(frequency_domain: numpy.ndarray, standard_table: numpy.ndarra
                     frequency_domain[i + u][j + v] //= standard_table[u][v]
 
 
-def encode(time_domain: numpy.ndarray, frequency_domain: numpy.ndarray) -> None:
+def encode(time_domain: numpy.ndarray, frequency_domain: numpy.ndarray,
+           standard_table: numpy.ndarray) -> None:
     for i in range(0, time_domain.shape[0], 8):
         for j in range(0, time_domain.shape[1], 8):
             dct(time_domain, frequency_domain, i, j)
+    quantification(frequency_domain, standard_table)
+    for i in range(0, frequency_domain.shape[0], 8):
+        for j in range(0, frequency_domain.shape[1], 8):
+            u = 1
+            v = -1
+            x = i
+            y = j
+            while 1:
+                if x == i + 7 and y == j + 7:
+                    break
+                x += u
+                y += v
 
 
 def main():
@@ -43,18 +56,49 @@ def main():
     data = numpy.asarray(image)
     print(type(data))
     luminance_time_domain = numpy.zeros([upAlign(len(data), 8), upAlign(len(data[0]), 8)], dtype=int)
-    chrominance_time_domain = numpy.zeros([upAlign((len(data) + 1) // 2), upAlign((len(data[0]) + 1) // 2)], dtype=int)
-    saturation_time_domain = numpy.zeros([upAlign((len(data) + 1) // 2), upAlign((len(data[0]) + 1) // 2)], dtype=int)
+    chrominance_time_domain = \
+        numpy.zeros([upAlign((len(data) + 1) // 2), upAlign((len(data[0]) + 1) // 2)], dtype=int)
+    saturation_time_domain = \
+        numpy.zeros([upAlign((len(data) + 1) // 2), upAlign((len(data[0]) + 1) // 2)], dtype=int)
     for i in range(0, len(data)):
         for j in range(0, len(data[i])):
             luminance_time_domain[i][j] = 0.229 * data[i][j][0] + 0.587 * data[i][j][1] + 0.114 * data[i][j][2]
             if i % 2 == 0 and j % 2 == 0:
-                chrominance_time_domain[i // 2][j // 2] = -0.1687 * data[i][j][0] - 0.3313 * data[i][j][1] + 0.5 * data[i][j][2] + 128
-                saturation_time_domain[i // 2][j // 2] = 0.5 * data[i][j][0] - 0.4187 * data[i][j][1] - 0.0813 * data[i][j][2] + 128
+                chrominance_time_domain[i // 2][j // 2] = \
+                    -0.1687 * data[i][j][0] - 0.3313 * data[i][j][1] + 0.5 * data[i][j][2] + 128
+                saturation_time_domain[i // 2][j // 2] = \
+                    0.5 * data[i][j][0] - 0.4187 * data[i][j][1] - 0.0813 * data[i][j][2] + 128
     luminance_frequency_domain = numpy.zeros([upAlign(len(data), 8), upAlign(len(data[0]), 8)], dtype=int)
-    chrominance_frequency_domain = numpy.zeros([upAlign((len(data) + 1) // 2), upAlign((len(data[0]) + 1) // 2)], dtype=int)
-    saturation_frequency_domain = numpy.zeros([upAlign((len(data) + 1) // 2), upAlign((len(data[0]) + 1) // 2)], dtype=int)
-    encode(luminance_time_domain, luminance_frequency_domain)
+    chrominance_frequency_domain = \
+        numpy.zeros([upAlign((len(data) + 1) // 2), upAlign((len(data[0]) + 1) // 2)], dtype=int)
+    saturation_frequency_domain = \
+        numpy.zeros([upAlign((len(data) + 1) // 2), upAlign((len(data[0]) + 1) // 2)], dtype=int)
+    luminance_quantification = numpy.array(
+        [[],
+         [],
+         [],
+         [],
+         [],
+         [],
+         [],
+         []]
+    )
+    chrominance_quantification = numpy.array(
+        [[],
+         [],
+         [],
+         [],
+         [],
+         [],
+         [],
+         []]
+    )
+    luminance_res = []
+    encode(luminance_time_domain, luminance_frequency_domain, luminance_quantification, luminance_res)
+    chrominance_res = []
+    encode(chrominance_time_domain, chrominance_frequency_domain, chrominance_quantification, chrominance_res)
+    saturation_res = []
+    encode(saturation_time_domain, saturation_frequency_domain, chrominance_quantification, saturation_res)
     save_img('images/out.bmp', luminance_time_domain)
 
 
